6765ff3af9a995d64559c15b4c7d7af780355c49
Mihir <168328729+m1dn1ghtMHR@users.noreply.github.com>
2024-05-14 22:22:17 +0530

    Add files via upload

---
+++ subsniper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+
+
+def print_red(text):
+    RED = '\033[31m'
+    RESET = '\033[0m'
+    print(RED + text + RESET)
+
+def banner():
+    banner = """
+░██████╗██╗░░░██╗██████╗░░██████╗███╗░░██╗██╗██████╗░███████╗██████╗░
+██╔════╝██║░░░██║██╔══██╗██╔════╝████╗░██║██║██╔══██╗██╔════╝██╔══██╗
+╚█████╗░██║░░░██║██████╦╝╚█████╗░██╔██╗██║██║██████╔╝█████╗░░██████╔╝
+░╚═══██╗██║░░░██║██╔══██╗░╚═══██╗██║╚████║██║██╔═══╝░██╔══╝░░██╔══██╗
+██████╔╝╚██████╔╝██████╦╝██████╔╝██║░╚███║██║██║░░░░░███████╗██║░░██║
+╚═════╝░░╚═════╝░╚═════╝░╚═════╝░╚═╝░░╚══╝╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝       
+    """
+    print(banner)
+    print_red("                                               by M1dn1ghtMHR")
+
+if __name__ == "__main__":
+    banner()
+    print("")
+
+def request(url):
+    try:
+        return requests.get("https://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+def main():
+    parser = argparse.ArgumentParser(description="Created by M1dn1ght-MHR")
+    parser.add_argument("-d", "--domain", help="Target domain (ex. google.com)", required=True)
+    parser.add_argument("-w", "--wordlist", help="Path of the wordlist")
+    args = parser.parse_args()
+
+    target = args.domain
+    wordlist_path = args.wordlist if args.wordlist else 'default.txt'
+
+    print("Domain:", target)
+    print("Wordlist:", wordlist_path)
+
+    with open(wordlist_path, 'r') as wordlist_file:
+        for line in wordlist_file:
+            subdomain = line.strip()
+            test_url = subdomain + '.' + target
+            response = request(test_url)
+            if response:
+                print("[+] Discovered Subdomain ----->", test_url)
+
+if __name__ == "__main__":
+    main()
